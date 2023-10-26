d6283ab339e680d1eed271a999eba8438ca9777c
Mohir <bahtisat442@gmail.com>
2023-10-26 15:41:01 +0500

    vite config rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,9 @@ export default defineConfig({
   build: {
     // generate manifest.json in outDir
     manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "./src/main.js",
+    },
   },
-})
+});
