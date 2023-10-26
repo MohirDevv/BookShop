7878440b311aac33b6f1f9cb5bbf4bfa481b3365
Mohir <bahtisat442@gmail.com>
2023-10-26 16:28:18 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [Vue()],
   build: {
     // generate manifest.json in outDir
     manifest: true,
